23d9afbcf40017bcd41cf70f957cc61b6b8875cd
stasldertyg-stack <stasldertyg@gmail.com>
2026-03-19 15:49:50 +0700

    Add files via upload

---
+++ HelloWorld.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class HelloWorld : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        //Вывод в консоль
+        print("Hello,World");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
